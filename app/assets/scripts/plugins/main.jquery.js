49822a74bdb26593aa349d5f02c171df9f4db2f1
Simon Eramo <simonramo@gmail.com>
2015-07-01 16:21:43 +1000

    fresh

--- app/assets/scripts/plugins/main.jquery.js
+++ app/assets/scripts/plugins/main.jquery.js
@@ -1,11 +1,2 @@
 $(document).ready(function(){
-  $('.o-burger').on('click',function(){
-    $(this).toggleClass('--opened');
-  });
-
-  $('.o-burger').on('click',function(){
-    $('.m-header__nav').slideToggle();
-  });
-
-
 });
